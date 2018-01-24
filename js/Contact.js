a5bb2edcfb5746eeea09f00a66b44d73fe0100ca
Ogla Siljko <oglasiljko@gmail.com>
2018-01-24 18:55:57 +0100

    initial commit

---
+++ js/Contact.js
@@ -0,0 +1,16 @@
+
+function myMap() {
+  var myCenter = new google.maps.LatLng(45.250620,19.838374);
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {center: myCenter, zoom: 15};
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+    
+
+google.maps.event.addDomListener(window, "resize", function() {
+   var center = map.getCenter();
+   google.maps.event.trigger(map, "resize");
+   map.setCenter(center); 
+}); 
+    }
